0ee2b711903fdfa911678fa2f35612b0c8f07937
Marius <marius@example.com>
2022-08-16 08:48:23 +0300

    obj iter

---
+++ js/objIter.js
@@ -0,0 +1,19 @@
+const person1 = {
+  name: 'Serbentautas',
+  surname: 'Bordiūras',
+  age: 22,
+};
+const person2 = {
+  name: 'Stalas',
+  surname: 'Komodauskas',
+  age: 21,
+};
+const person3 = {
+  name: 'Janina',
+  surname: 'Joninaitė',
+  age: 42,
+};
+const people = [person1, person2, person3];
+
+// parasyti funkcija kuri iteruoja per people masyva ir atvaizduoja visas keys ir values html li viduje
+// nenaudoti Object.keys/entries/values
